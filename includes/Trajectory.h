dc626d196c077f147056cb8c929871981dc739e0
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2018-05-20 19:54:35 +0300

    started the new project repository

---
+++ includes/Trajectory.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <memory>
+#include "motion/MotionProfile.h"
+#include "path/Path.h"
+
+/*
+A Trajectory Point(or TrajPoint) combines info about a MotionProfile Setpoint and a Path Waypoint
+and allows access specific to information for following the trajectory
+*/
+
+struct TrajPoint {
+private:
+	Setpoint m_sp;
+	Waypoint m_wp;
+
+public:
+	TrajPoint(const Setpoint& sp, const Waypoint& wp);
+
+	//Setpoint interface
+	const double GetPos() const;
+	const double GetVelocity() const;
+	const double GetAcceleration() const;
+	const double GetTime() const;
+
+	//Waypoint interface
+	const double GetHeading() const;
+	const double GetHeadingDegrees() const;
+
+};
+
+class Trajectory {
+private:
+	Path m_path;
+	double m_time;
+	MotionProfileConfig m_config;
+	MotionProfile m_profile;
+
+	bool m_reversed;
+public:
+	Trajectory(const Path& p, const MotionProfileConfig& config, double time = 0, bool reversed = false);
+
+	const MotionProfileConfig& GetConfig() const;
+
+	const TrajPoint GetTrajPointT(double t);
+	const TrajPoint GetTrajPointD(double d);
+
+	const MotionProfile& GetProfile() const;
+	const Path& GetPath() const;
+	const std::vector<Waypoint>& GetWaypoints() const;
+
+	const double GetDistance() const;
+	const double GetTime() const;
+	const bool IsReversed() const;
+};
+
